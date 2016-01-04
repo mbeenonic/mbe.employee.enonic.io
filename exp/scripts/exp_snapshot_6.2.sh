25db3059e5728709a1228223e14e9bdbdc9b6550
Marek Bettman <mbe@enonic.com>
2016-01-04 14:49:32 +0100

    work in progress

--- exp/scripts/exp_snapshot_6.2.sh
+++ exp/scripts/exp_snapshot_6.2.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+##########
+# CONFIG #
+##########
+
 ADMIN_USER=$1
 ADMIN_PASSWORD=$2
+SNAPSHOT_LOCATION=${XP_INSTALL}/home/data/snapshot
+BACKUP_FILE=/tmp/backup.tar.gz
+
+#############
+# FUNCTIONS #
+#############
 
 function _error {
 	echo -e "[ERROR] $1\n"
 }
 
 function _info {
-	echo "[INFO] $1"
+	echo "$1"
 }
+
+########
+# MAIN #
+########
 
 if [ -z "$XP_INSTALL" ]; then
     _error '$XP_INSTALL not set'
@@ -22,8 +36,22 @@ if [ ! -f ${XP_INSTALL}/toolbox/toolbox.sh ]; then
     exit 1
 fi
 
-_info "$XP_INSTALL/toolbox/toolbox.sh -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r cms-repo"
-$XP_INSTALL/toolbox/toolbox.sh -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r cms-repo
+_info "$XP_INSTALL/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r cms-repo"
+$XP_INSTALL/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r cms-repo
 
-_info "$XP_INSTALL/toolbox/toolbox.sh -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r system-repo"
-$XP_INSTALL/toolbox/toolbox.sh -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r system-repo
+_info "$XP_INSTALL/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r system-repo"
+$XP_INSTALL/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r system-repo
+
+_info $(ls -la ${XP_INSTALL}/home/data/snapshot)
+
+if [ -f $BACKUP_FILE ]; then
+    _info "Found old version of $BACKUP_FILE - removing"
+    rm $BACKUP_FILE
+fi
+
+tar cpfz $BACKUP_FILE $SNAPSHOT_LOCATION &> /dev/null
+
+if [ -f $BACKUP_FILE ]; then
+    _info "$BACKUP_FILE generated successfully"
+    _info $(ls -la /tmp)
+fi
--- exp/scripts/exp_snapshot_6.3.sh
+++ exp/scripts/exp_snapshot_6.3.sh
@@ -1,29 +1,54 @@
 #!/bin/bash
 
-ADMIN_USER=$2
+##########
+# CONFIG #
+##########
+
+ADMIN_USER=$1
 ADMIN_PASSWORD=$2
+SNAPSHOT_LOCATION=${XP_INSTALL}/home/snapshots
+BACKUP_FILE=/tmp/backup.tar.gz
+
+#############
+# FUNCTIONS #
+#############
 
 function _error {
 	echo -e "[ERROR] $1\n"
 }
 
 function _info {
-	echo "[INFO] $1"
+	echo "$1"
 }
+
+########
+# MAIN #
+########
 
 if [ -z "$XP_INSTALL" ]; then
     _error '$XP_INSTALL not set'
     exit 1
 fi
 
-if [ -f $XP_INSTALL/toolbox/toolbox.sh ]; then
-    _error '$XP_INSTALL/toolbox/toolbox.sh does not exist'
+if [ ! -f ${XP_INSTALL}/toolbox/toolbox.sh ]; then
+    _error "$XP_INSTALL/toolbox/toolbox.sh does not exist"
+    _info $(ls -la $XP_INSTALL)
+    exit 1
 fi
 
-_info "Creating new snapshot for XP version $XP_VERSION"
-if [ "$XP_VERSION" == '6.3' ]; then
-    /bin/bash $XP_INSTALL/toolbox/toolbox.sh -a ${ADMIN_USER}:${ADMIN_USER}
-elif [ "$XP_VERSION" == '6.2' ]; then
-    /bin/bash $XP_INSTALL/toolbox/toolbox.sh -a ${ADMIN_USER}:${ADMIN_USER} -r cms-repo
-    /bin/bash $XP_INSTALL/toolbox/toolbox.sh -a ${ADMIN_USER}:${ADMIN_USER} -r system-repo
+_info "$XP_INSTALL/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD}"
+$XP_INSTALL/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD}
+
+_info $(ls -la ${XP_INSTALL}/home/snapshots)
+
+if [ -f $BACKUP_FILE ]; then
+    _info "Found old version of $BACKUP_FILE - removing"
+    rm $BACKUP_FILE
+fi
+
+tar cpfz $BACKUP_FILE $SNAPSHOT_LOCATION &> /dev/null
+
+if [ -f $BACKUP_FILE ]; then
+    _info "$BACKUP_FILE generated successfully"
+    _info $(ls -la /tmp)
 fi
