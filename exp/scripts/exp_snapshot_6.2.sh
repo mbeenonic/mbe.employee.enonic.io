dedb875b0cee71cb02ccec5dcc35dab55e5dbbfd
Marek Bettman <mbe@enonic.com>
2016-01-11 14:52:08 +0100

    work in progress

--- exp/scripts/exp_snapshot_6.2.sh
+++ exp/scripts/exp_snapshot_6.2.sh
@@ -7,10 +7,10 @@
 ADMIN_USER=$1
 ADMIN_PASSWORD=$2
 
-SNAPSHOT_LOCATION=${XP_ROOT}/home/data/snapshot
-BLOBS_LOCATION=${XP_ROOT}/home/repo/blob
+#SNAPSHOT_LOCATION=${XP_ROOT}/home/data/snapshot
+#BLOBS_LOCATION=${XP_ROOT}/home/repo/blob
 #BACKUP_FILE=/tmp/backup.tar.gz
-BACKUP_DIR=/tmp/backup
+#BACKUP_DIR=/tmp/backup
 
 #############
 # FUNCTIONS #
@@ -50,18 +50,18 @@ $XP_ROOT/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r syste
 #    rm $BACKUP_FILE
 #fi
 
-if [ ! -d $BACKUP_DIR ]; then
-    _info "$BACKUP_DIR does not exist - creating"
-    mkdir $BACKUP_DIR
-fi
+#if [ ! -d $BACKUP_DIR ]; then
+#    _info "$BACKUP_DIR does not exist - creating"
+#    mkdir $BACKUP_DIR
+#fi
 
 #tar cpfz $BACKUP_FILE $SNAPSHOT_LOCATION $BLOBS_LOCATION &> /dev/null
 
-_info "Copying $SNAPSHOT_LOCATION to $BACKUP_DIR"
-cp -pR $SNAPSHOT_LOCATION $BACKUP_DIR &> /dev/null
+#_info "Copying $SNAPSHOT_LOCATION to $BACKUP_DIR"
+#cp -pR $SNAPSHOT_LOCATION $BACKUP_DIR &> /dev/null
 
-_info "Copying $BLOBS_LOCATION to $BACKUP_DIR"
-cp -pR $BLOBS_LOCATION $BACKUP_DIR &> /dev/null
+#_info "Copying $BLOBS_LOCATION to $BACKUP_DIR"
+#cp -pR $BLOBS_LOCATION $BACKUP_DIR &> /dev/null
 
 #if [ -f $BACKUP_FILE ]; then
 #    _info "$BACKUP_FILE generated successfully"
