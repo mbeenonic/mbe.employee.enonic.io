a8514de06af633bab18d4bc502d578b199418f29
Marek Bettman <mbe@enonic.com>
2016-01-11 13:22:10 +0100

    work in progress

--- exp/scripts/exp_snapshot_6.2.sh
+++ exp/scripts/exp_snapshot_6.2.sh
@@ -6,9 +6,11 @@
 
 ADMIN_USER=$1
 ADMIN_PASSWORD=$2
-SNAPSHOT_LOCATION=${XP_INSTALL}/home/data/snapshot
-BLOBS_LOCATION=${XP_INSTALL}/home/repo/blob
-BACKUP_FILE=/tmp/backup.tar.gz
+
+SNAPSHOT_LOCATION=${XP_ROOT}/home/data/snapshot
+BLOBS_LOCATION=${XP_ROOT}/home/repo/blob
+#BACKUP_FILE=/tmp/backup.tar.gz
+BACKUP_DIR=/tmp/backup
 
 #############
 # FUNCTIONS #
@@ -26,31 +28,41 @@ function _info {
 # MAIN #
 ########
 
-if [ -z "$XP_INSTALL" ]; then
-    _error '$XP_INSTALL not set'
+if [ -z "$XP_ROOT" ]; then
+    _error '$XP_ROOT not set'
     exit 1
 fi
 
-if [ ! -f ${XP_INSTALL}/toolbox/toolbox.sh ]; then
-    _error "$XP_INSTALL/toolbox/toolbox.sh does not exist"
-    _info $(ls -la $XP_INSTALL)
+if [ ! -f ${XP_ROOT}/toolbox/toolbox.sh ]; then
+    _error "$XP_ROOT/toolbox/toolbox.sh does not exist"
+    _info $(ls -la $XP_ROOT)
     exit 1
 fi
 
-_info "Running $XP_INSTALL/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r cms-repo"
-$XP_INSTALL/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r cms-repo
+_info "Running $XP_ROOT/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r cms-repo"
+$XP_ROOT/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r cms-repo
 
-_info "Running $XP_INSTALL/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r system-repo"
-$XP_INSTALL/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r system-repo
+_info "Running $XP_ROOT/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r system-repo"
+$XP_ROOT/toolbox/toolbox.sh snapshot -a ${ADMIN_USER}:${ADMIN_PASSWORD} -r system-repo
 
+#if [ -f $BACKUP_FILE ]; then
+#    _info "Found old version of $BACKUP_FILE - removing"
+#    rm $BACKUP_FILE
+#fi
 
-if [ -f $BACKUP_FILE ]; then
-    _info "Found old version of $BACKUP_FILE - removing"
-    rm $BACKUP_FILE
+if [ ! -d $BACKUP_DIR ]; then
+    _info "$BACKUP_DIR does not exist - creating"
+    mkdir $BACKUP_DIR
 fi
 
-tar cpfz $BACKUP_FILE $SNAPSHOT_LOCATION $BLOBS_LOCATION &> /dev/null
+#tar cpfz $BACKUP_FILE $SNAPSHOT_LOCATION $BLOBS_LOCATION &> /dev/null
 
-if [ -f $BACKUP_FILE ]; then
-    _info "$BACKUP_FILE generated successfully"
-fi
+_info "Copying $SNAPSHOT_LOCATION to $BACKUP_DIR"
+cp -pR $SNAPSHOT_LOCATION $BACKUP_DIR &> /dev/null
+
+_info "Copying $BLOBS_LOCATION to $BACKUP_DIR"
+cp -pR $BLOBS_LOCATION $BACKUP_DIR &> /dev/null
+
+#if [ -f $BACKUP_FILE ]; then
+#    _info "$BACKUP_FILE generated successfully"
+#fi
